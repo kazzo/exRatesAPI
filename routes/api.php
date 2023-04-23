430de5b8d8e658f4c5f8508489dc29e8e8cec884
kazzo <gm.kazmierczak@gmail.com>
2023-04-23 23:12:28 +0200

    amendments

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ExchangeRateController;
-//use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\LoginController;
 /*
  * |--------------------------------------------------------------------------
@@ -19,11 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
     
-//Route::post('login', [AuthController::class, 'login']);
 Route::post('login', [LoginController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
-    //Route::post('logout', [AuthController::class, 'logout']);
     Route::post('logout', [LoginController::class, 'logout']);
     
     Route::get('exchangerate/{date}', [ExchangeRateController::class, 'index']);
@@ -32,7 +29,3 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::post('exchangerate', [ExchangeRateController::class, 'store']);
 });
 
-//Route::apiResource('exchangerate/{date}', [ExchangeRateController::class, 'index'])->middleware('auth:sanctum');
-//Route::apiResource('exchangerate/{date}/{currency}', [ExchangeRateController::class, 'index'])->middleware('auth:sanctum');
-
-//Route::post('exchangerate', 'ExchangeRateController@store');
