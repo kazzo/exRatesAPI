ddfe0c084f25a31a4fff2c02a9b10e39c6b34bf2
kazzo <gm.kazmierczak@gmail.com>
2023-04-23 23:37:21 +0200

    removed some commented code

--- app/Http/Controllers/Api/ExchangeRateController.php
+++ app/Http/Controllers/Api/ExchangeRateController.php
@@ -10,7 +10,6 @@ use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-//use App\Http\Requests\ExchangeRateIndexRequest;
 use App\Http\Requests\ExchangeRateIndexRequest;
 use App\Http\Requests\ExchangeRateStoreRequest;
 
@@ -18,7 +17,7 @@ class ExchangeRateController extends Controller
 {
     public function index(ExchangeRateIndexRequest $request, string $date, string $currency='')
     {
-        try{
+        try {
             return response()->json(ExchangeRate::getRates($date, $currency), 200);
         } catch (\Throwable $th) {
             return response()->json(['status' => false, 'message' => $th->getMessage()], 500);
@@ -28,21 +27,7 @@ class ExchangeRateController extends Controller
        
     public function store(ExchangeRateStoreRequest $request)
     {
-/*        
-        $user = Auth::guard('api')->user();
-        if(!in_array($user->role , 	['ADMIN']))
-            return response()->json(['message' => 'You not have right to access this resource'], 403);
-        
-        $request->validate([
-            'currency' => 'required|alpha|min:3|max:3',            
-            'amount' => 'required|numeric|gt:0',
-            'date' => 'required|date|date_format:Y-m-d|before:tomorrow'
-        ]);
-        
-        $request->merge(['currency' => trim(strtoupper($request->input('currency')))]);
-        $request->merge(['quantity' => $request->input('quantity') ?? 1]);        
-*/
-        try{
+        try {
             return response()->json(ExchangeRate::addRate($request), 200);
         } catch (\Throwable $th) {
             return response()->json(['status' => false, 'message' => $th->getMessage()], 500);
